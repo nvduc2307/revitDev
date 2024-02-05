7c36167d5f7e1cbfd65ec272a2254a5b9b4fcd2f
nvduc2307 <nvduc.yt@gmail.com>
2024-02-05 15:43:57 +0700

    update-extension-filter-element

--- FilterElements/FilterElement.cs
+++ FilterElements/FilterElement.cs
@@ -6,7 +6,53 @@ namespace RevitDev.FilterElements
 {
     public static class FilterElement
     {
-        public static List<T> GetElements<T>(this Document document, bool isType)
+        public static List<T> GetElementsFromCategory<T>(this Document document, View view, BuiltInCategory builtInCategory, bool isType)
+        {
+            var results = new List<T>();
+            if (isType)
+            {
+                results = new FilteredElementCollector(document, view.Id)
+                .WhereElementIsElementType()
+                .OfCategory(builtInCategory)
+                .Cast<T>()
+                .Where(x => x != null)
+                .ToList();
+            }
+            else
+            {
+                results = new FilteredElementCollector(document, view.Id)
+                .WhereElementIsNotElementType()
+                .OfCategory(builtInCategory)
+                .Cast<T>()
+                .Where(x => x != null)
+                .ToList();
+            }
+            return results;
+        }
+        public static List<T> GetElementsFromCategory<T>(this Document document, BuiltInCategory builtInCategory, bool isType)
+        {
+            var results = new List<T>();
+            if (isType)
+            {
+                results = new FilteredElementCollector(document)
+                .WhereElementIsElementType()
+                .OfCategory(builtInCategory)
+                .Cast<T>()
+                .Where(x => x != null)
+                .ToList();
+            }
+            else
+            {
+                results = new FilteredElementCollector(document)
+                .WhereElementIsNotElementType()
+                .OfCategory(builtInCategory)
+                .Cast<T>()
+                .Where(x => x != null)
+                .ToList();
+            }
+            return results;
+        }
+        public static List<T> GetElementsFromClass<T>(this Document document, bool isType)
         {
             var results = new List<T>();
             if (isType)
@@ -15,6 +61,7 @@ namespace RevitDev.FilterElements
                 .WhereElementIsElementType()
                 .OfClass(typeof(T))
                 .Cast<T>()
+                .Where(x => x != null)
                 .ToList();
             }
             else
@@ -23,7 +70,49 @@ namespace RevitDev.FilterElements
                 .WhereElementIsNotElementType()
                 .OfClass(typeof(T))
                 .Cast<T>()
+                .Where(x => x != null)
                 .ToList();
+            }
+            return results;
+        }
+        public static List<T> GetElementsFromClass<T>(this Document document)
+        {
+            var results = new List<T>();
+            if (typeof(T) != typeof(View) && typeof(T) != typeof(View3D))
+            {
+                results = new FilteredElementCollector(document)
+                .OfClass(typeof(T))
+                .Cast<T>()
+                .Where(x => x != null)
+                .ToList();
+            }
+            return results;
+        }
+        public static List<T> GetViewsFromClass<T>(this Document document, bool isTemplate)
+        {
+            var results = new List<T>();
+            if (typeof(T) == typeof(View) || typeof(T) == typeof(View3D))
+            {
+                if (isTemplate)
+                {
+                    var views = new FilteredElementCollector(document)
+                    .OfClass(typeof(View))
+                    .Cast<View>()
+                    .Where(x => x != null)
+                    .Where(x => x.IsTemplate)
+                    .ToList();
+                    results = views as List<T>;
+                }
+                else
+                {
+                    var views = new FilteredElementCollector(document)
+                    .OfClass(typeof(View))
+                    .Cast<View>()
+                    .Where(x => x != null)
+                    .Where(x => !x.IsTemplate)
+                    .ToList();
+                    results = views as List<T>;
+                }
             }
             return results;
         }
